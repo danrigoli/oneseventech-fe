093b8a56b31eeca0b6f85c375efa6c0972e96e96
danrigoli <danterigoli02@gmail.com>
2022-09-17 21:30:14 -0300

    finished checkout done

--- src/app/checkout/checkout-routing.module.ts
+++ src/app/checkout/checkout-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CheckoutDoneComponent } from './checkout-done/checkout-done.component';
 import { CheckoutViewComponent } from './checkout-view/checkout-view.component';
 import { PaymentExistsGuard } from './guards/payment-exists/payment-exists.guard';
+import { PaymentSuccessfulGuard } from './guards/payment-successful/payment-successful.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products/list', pathMatch: 'full' },
-  { path: 'success/:id', component: CheckoutViewComponent },
+  { path: 'success/:id', component: CheckoutDoneComponent, canActivate: [PaymentSuccessfulGuard] },
   { path: ':id', component: CheckoutViewComponent, canActivate: [PaymentExistsGuard] },
 ];
 
