411b96516c4d10aef31edadfb08c6fb0eba43013
danrigoli <danterigoli02@gmail.com>
2022-09-18 13:08:55 -0300

    added sorting && loading cards

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -12,6 +12,8 @@ import { ToastrService } from 'ngx-toastr';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = []
+  numbers = Array(8).fill(0).map((x,i)=>i);
+  sort = SortValues.Relevance.toString();
 
   constructor(private apiService: ApiService, private toastr: ToastrService) { }
 
@@ -31,4 +33,31 @@ export class ProductListComponent implements OnInit {
     this.apiService.getProducts().subscribe((data) => this.products = data);
   }
 
+  sortProducts() {
+    switch (this.sort) {
+      case SortValues.PriceLowToHigh.toString():        
+        this.products.sort((a, b) => a.price - b.price);
+        break;
+      case SortValues.PriceHighToLow.toString():
+        this.products.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        this.products.sort((a, b) => a.id - b.id);
+        break;
+    }
+
+    
+  }
+
+  get sortValues(): typeof SortValues {
+    return SortValues; 
+  }
+
 }
+
+enum SortValues {
+  Relevance = 0,
+  PriceLowToHigh = 1,
+  PriceHighToLow = 2,
+}
+
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,12 +4,13 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ApiService } from './services/api.service';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ ProductListComponent, ProductCardComponent ],
-  imports: [ CommonModule, ProductsRoutingModule],
+  imports: [ CommonModule, ProductsRoutingModule, FormsModule],
   providers: [ ApiService ]
 })
 export class ProductsModule { }
